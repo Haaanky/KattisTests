097777ec6931f9cc7bb9050322b01ceadffd0da8
Haaanky <Haaanky@gmail.com>
2019-05-21 11:41:43 +0200

    Add project files.

---
+++ KattisTestsAcademicWorkFramework46/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string line;
+        int i = 0;
+        while ((line = Console.ReadLine()) != null)
+        {
+            string[] split = line.Split(' ');
+            int startDayEarth = int.Parse(split[0]);
+            int startDayMars = int.Parse(split[1]);
+            int daysPast = 0;
+            for (int e = startDayEarth, m = startDayMars; !(e == 0 && m == 0);)
+            {
+                daysPast++;
+                e++;
+                m++;
+                if (e == 365)
+                    e = 0;
+                if (m == 687)
+                    m = 0;
+            }
+            i++;
+            Console.WriteLine($"Case {i}: {daysPast}");
+        }
+    }
+}
