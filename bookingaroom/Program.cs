702481dadd441944f67decb220c14f2ac37d2823
Henrik Norlin Frisemo <haaanky@gmail.com>
2019-06-10 11:04:35 +0200

    commit

---
+++ bookingaroom/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bookingaroom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
